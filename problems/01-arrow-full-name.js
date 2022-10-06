32324a4f59a9f8c2e4b06b1143d98ffddd1eb236
kutun0901 <tuan.tq2433@gmail.com>
2022-10-06 13:27:14 -0700

    02 solved

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -15,7 +15,12 @@ arrowGetFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
 
 // Your code here
+const arrowGetFullName = person => {
+  return person.firstName + " " + person.lastName;
+}
 
+let p1 = {firstName: 'John', lastName: 'Doe'};
+console.log(arrowGetFullName(p1)); // => 'John Doe'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
